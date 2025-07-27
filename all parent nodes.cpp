ef90a4117fbe58ac996979c8ace76dfbd25fcdb2
Hasaan Ahmad <hasaanahmadn6@gmail.com>
2025-07-27 16:12:17 +0500

    Add files via upload

---
+++ all parent nodes.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+
+class node {
+public:
+    int data;
+    node* left;
+    node* right;
+};
+
+class tree {
+public:
+    node* root;
+
+    node* createnode(int data) {
+        node* newnode = new node();
+        newnode->data = data;
+        newnode->left = newnode->right = NULL;
+        return newnode;
+    }
+
+    node* insert(node* root, int data) {
+        if (root == NULL) {
+            root = createnode(data);
+        }
+        else if (data < root->data) {
+            root->left = insert(root->left, data);
+        }
+        else {
+            root->right = insert(root->right, data);
+        }
+        return root;
+    }
+
+    // Function to print all parent nodes (i.e., nodes with at least one child)
+    void printParentNodes(node* root) {
+        if (root == NULL) {
+            return;
+        }
+
+        // If node has at least one child, it's a parent
+        if (root->left != NULL || root->right != NULL) {
+            cout << root->data << " ";
+        }
+
+        printParentNodes(root->left);
+        printParentNodes(root->right);
+    }
+};
+
+int main() {
+    node* root = NULL;
+    tree t;
+    root = t.insert(root, 15);
+    t.insert(root, 10);
+    t.insert(root, 50);
+    t.insert(root, 51);
+
+    cout << "Parent nodes: ";
+    t.printParentNodes(root);
+    cout << endl;
+
+    return 0;
+}
+
